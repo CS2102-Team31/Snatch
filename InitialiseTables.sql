fd80aff84ec8fb7c75347666214c2d7ced5713ac
CindyTsai1 <cindy1.tsai@gmail.com>
2018-02-28 12:29:02 +0800

    Update query due to restrictions on pgadmin

--- InitialiseTables.sql
+++ InitialiseTables.sql
@@ -1,13 +1,14 @@
 -- Users
 CREATE TABLE users (
-    email VARCHAR(32) primary key,
-    userid INTEGER primary key,
+    email VARCHAR(32),
+    userid INTEGER,
     username VARCHAR(32) unique not null,
     pwd VARCHAR(32) not null,
     phone numeric(8, 0) not null,
     gender VARCHAR(6),
     bday DATE,
     driverLicense VARCHAR(9),
+    primary key(email, userid),
     check(email like '___%@%___.com' and username not like '%[0-9]%')
 );
 
@@ -26,42 +27,47 @@ CREATE TABLE rides (
 
 -- Cars
 CREATE TABLE cars (
-    carid INTEGER primary key,
-    licensePlate VARCHAR(8) primary key,
+    carid INTEGER,
+    licensePlate VARCHAR(8),
     carType VARCHAR(32) not null,
+    primary key(carid, licensePlate),
     check(licensePlate like '[A-Z][A-Z][A-Z][0-9][0-9][0-9][0-9][A-Z]')
 );
 
 -- Admins
 CREATE TABLE admins (
-    adminid INTEGER primary key,
-    adminname VARCHAR(32) primary key,
+    adminid INTEGER,
+    adminname VARCHAR(32),
     adminpwd VARCHAR(32) not null,
     employeename VARCHAR(32) not null
+    primary key(adminid, adminname)
 );
 
 -- User owns cars
 CREATE TABLE owns (
-    userid INTEGER primary key,
-    carid INTEGER primary key
+    userid INTEGER,
+    carid INTEGER,
+    primary key(userid, carid)
 );
 
 -- User drives rides
 CREATE TABLE drives (
-    userid INTEGER primary key,
-    dates DATE primary key,
-    times TIME primary key,
+    userid INTEGER,
+    dates DATE,
+    times TIME,
     rideid INTEGER unique not null,
-    carid INTEGER not null
+    carid INTEGER not null,
+    primary key(userid, dates, times)
 );
 
 -- User bids for rides
 CREATE TABLE bids (
-    userid INTEGER primary key,
-    rideid INTEGER primary key,
+    userid INTEGER,
+    rideid INTEGER,
     price INTEGER not null,
     status VARCHAR(1) not null,
-    sidenote TEXT
+    sidenote TEXT,
+    primary key(userid, rideid)
 );
 
 -- Administrator manages entities
