1748633a99f770105c5894a75bb58ed2cc1cef66
Yilun Zhu <yilun95@gmail.com>
2018-04-03 16:49:29 +0800

    add bid constraint

--- Webpages/bid.php
+++ Webpages/bid.php
@@ -3,150 +3,155 @@
   <title>UPDATE PostgreSQL data with PHP</title>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <style>
-      .error {
-          color: #FF0000;
-      }
+  .error {
+    color: #FF0000;
+  }
 
-      li {
-          list-style: none;
-      }
+  li {
+    list-style: none;
+  }
   </style>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
 </head>
 <body>
   <nav class="navbar navbar-dark" style="background-color: coral;">
-  <a class="navbar-brand mx-auto" href="index.php">
+    <a class="navbar-brand mx-auto" href="index.php">
       Snatch
-  </a>
+    </a>
   </nav>
 
   <div>
 
-  <br><h1 style="text-align: center">Ride bidding info</h1> <br>
+    <br><h1 style="text-align: center">Ride bidding info</h1> <br>
 
-  <?php
-  // Connect to the database. Please change the password in the following line accordingly
+    <?php
+    // Connect to the database. Please change the password in the following line accordingly
 
-  include 'phpconfig.php';
-  session_start();
-  $userID = $_SESSION['userID'];
-  $db     = $psql;
-  $result = pg_query($db, "with bidcount as (
-  SELECT ridesid, status, min(price) as minprice, count(*) as numbids
-  FROM bids
-  Where status = '0'
-  GROUP By ridesid, status
-  )
+    include 'phpconfig.php';
+    session_start();
+    $userID = $_SESSION['userID'];
+    $db     = $psql;
+    $result = pg_query($db, "with bidcount as (
+    SELECT ridesid, status, min(price) as minprice, count(*) as numbids
+    FROM bids
+    Where status = '0'
+    GROUP By ridesid, status
+    )
 
-  SELECT R.rideid, R.dates, R.times, R.origin, R.destination, R.baseprice, R.capacity, coalesce(B.numbids,0) as numBids, coalesce(B.minprice, 0) as minBid
-  FROM rides R full outer join bidcount B on (R.rideid = B.ridesid)
-  Order by R.dates, R.times, R.origin, R.destination, minbid
-  ;
+    SELECT R.rideid, R.dates, R.times, R.origin, R.destination, R.baseprice, R.capacity, coalesce(B.numbids,0) as numBids, coalesce(B.minprice, 0) as minBid
+    FROM rides R full outer join bidcount B on (R.rideid = B.ridesid)
+    Order by R.dates, R.times, R.origin, R.destination, minbid
+    ;
 
-  ");
-  // print out table of bids
-  echo 'Origin:<input type="text" class="form-control" id="myOrigin" onkeyup="bidFilter()" placeholder="Filter by origin..">';
-  echo 'Destination:<input type="text" class="form-control" id="myDest" onkeyup="bidFilter()" placeholder="Filter by destination.."> <br>';
+    ");
+    // print out table of bids
+    echo 'Origin:<input type="text" class="form-control" id="myOrigin" onkeyup="bidFilter()" placeholder="Filter by origin..">';
+    echo 'Destination:<input type="text" class="form-control" id="myDest" onkeyup="bidFilter()" placeholder="Filter by destination.."> <br>';
 
-  $i = 0;
-  echo '<table id = "myTable" class= "table"><tr>';
-  while ($i < pg_num_fields($result))
-  {
-    $fieldName = pg_field_name($result, $i);
-    echo '<td>' . $fieldName . '</td>';
-    $i = $i + 1;
-  }
-  echo '</tr>';
-  $i = 0;
-
-  while ($row = pg_fetch_row($result))
-  {
-    echo '<tr>';
-    $count = count($row);
-    $y = 0;
-    while ($y < $count)
+    $i = 0;
+    echo '<table id = "myTable" class= "table"><tr>';
+    while ($i < pg_num_fields($result))
     {
-      $c_row = current($row);
-      echo '<td>' . $c_row . '</td>';
-      next($row);
-      $y = $y + 1;
+      $fieldName = pg_field_name($result, $i);
+      echo '<td>' . $fieldName . '</td>';
+      $i = $i + 1;
     }
     echo '</tr>';
-    $i = $i + 1;
-  }
-  pg_free_result($result);
+    $i = 0;
 
-  echo '</table>';
-
-  ?>
-</div>
-<div style="text-align: center">
-  <h1 style="text-align: center"> Bid for a ride </h1>
-
-
-  <form name="form1" id="form1" action="" method="POST">
-    <select name='rideid' id = 'rideid' >
-      <option value="">--- Select Ride ID ---</option>
-      <?php
-      // This is the db to connect to
-      include 'phpconfig.php';
-      $db     = $psql;
-      $result = pg_query($db, "SELECT rideid FROM rides");		// Query template
-
-
-      while ($row = pg_fetch_array($result)) {
-        echo "<option value='" . $row['rideid'] ."'>" . $row['rideid'] ."</option>";
+    while ($row = pg_fetch_row($result))
+    {
+      echo '<tr>';
+      $count = count($row);
+      $y = 0;
+      while ($y < $count)
+      {
+        $c_row = current($row);
+        echo '<td>' . $c_row . '</td>';
+        next($row);
+        $y = $y + 1;
       }
-      ?>
-    </select>
-    <input type="submit" style="text-align: center" value="Select Ride ID" name="submitRideId"><br>
-  </form>
-
-  <?php
-  // This is the db to connect to
-  include 'phpconfig.php';
-  $db     = $psql;
-  session_start();
-  $userid = $_SESSION['userID'];
-  $result = pg_query($db, "SELECT * FROM rides where rideid = '$_POST[rideid]'");		// Query template
-  $row    = pg_fetch_assoc($result);		// To store the result row
-
-  if (isset($_POST['submitRideId'])) {
-
-    echo "
-    <form name='bid' method='POST'>
-    RideID:<br>
-    <input type='text' name='rideid' value='$row[rideid]'><br>
-    Date:<br>
-    <input type='text' name='dates' value='$row[dates]' disabled><br>
-    Time:<br>
-    <input type='text' name='times' value='$row[times]' disabled><br>
-    Origin:<br>
-    <input type='text' name='origin' value='$row[origin]' disabled><br>
-    Destination:<br>
-    <input type='text' name='destination' value='$row[destination]' disabled><br>
-    Your bid:<br>
-    <input type='text' name='bid' value='--Enter your bid--'><br>
-    UserID:<br>
-    <input type='text' name='userid' value='$userID'><br>
-    <input type='submit' name='new'><br>
-    </form>
-    ";
-  }
-  if (isset($_POST['new'])) {	// Submit the update SQL command, update if user has already bid for ride, else insert.
-    $result = pg_query($db, " UPDATE bids SET price = '$_POST[bid]' WHERE usersid = '$_POST[userid]' and ridesid = '$_POST[rideid]' ;
-      INSERT INTO bids
-      SELECT '$_POST[userid]', '$_POST[rideid]', '$_POST[bid]', 0, null
-      WHERE NOT EXISTS (SELECT 1 FROM bids WHERE usersid = '$_POST[userid]' and ridesid = '$_POST[rideid]');");
-      if (!$result) {
-        echo "Bid failed!!";
-      } else {
-        echo "Bid successful!";
-      }
+      echo '</tr>';
+      $i = $i + 1;
     }
+    pg_free_result($result);
+
+    echo '</table>';
 
     ?>
   </div>
+  <div style="text-align: center">
+    <h1 style="text-align: center"> Bid for a ride </h1>
+
+
+    <form name="form1" id="form1" action="" method="POST">
+      <select name='rideid' id = 'rideid' >
+        <option value="">--- Select Ride ID ---</option>
+        <?php
+        // This is the db to connect to
+        include 'phpconfig.php';
+        $db     = $psql;
+        $result = pg_query($db, "SELECT rideid FROM rides");		// Query template
+
+
+        while ($row = pg_fetch_array($result)) {
+          echo "<option value='" . $row['rideid'] ."'>" . $row['rideid'] ."</option>";
+        }
+        ?>
+      </select>
+      <input type="submit" style="text-align: center" value="Select Ride ID" name="submitRideId"><br>
+    </form>
+
+    <?php
+    // This is the db to connect to
+    include 'phpconfig.php';
+    $db     = $psql;
+    session_start();
+    $userid = $_SESSION['userID'];
+    $result = pg_query($db, "SELECT * FROM rides where rideid = '$_POST[rideid]'");		// Query template
+    $row    = pg_fetch_assoc($result);		// To store the result row
+
+    if (isset($_POST['submitRideId'])) {
+
+      echo "
+      <form name='bid' method='POST'>
+      RideID:<br>
+      <input type='text' name='rideid' value='$row[rideid]' readonly><br>
+      Date:<br>
+      <input type='text' name='dates' value='$row[dates]' readonly><br>
+      Time:<br>
+      <input type='text' name='times' value='$row[times]' readonly><br>
+      Origin:<br>
+      <input type='text' name='origin' value='$row[origin]' readonly><br>
+      Destination:<br>
+      <input type='text' name='destination' value='$row[destination]' readonly><br>
+      Base price:<br>
+      <input type='text' name='baseprice' value='$row[baseprice]' readonly><br>
+      Your bid:<br>
+      <input type='text' name='bid' value='--Enter your bid--'><br>
+      <input type='submit' name='new'><br>
+      </form>
+      ";
+    }
+    if (isset($_POST['new'])) {	// Submit the update SQL command, update if user has already bid for ride, else insert.
+      if($_POST[bid] >= $_POST[baseprice]) {
+        $result = pg_query($db, " UPDATE bids SET price = '$_POST[bid]' WHERE usersid = '$userid' and ridesid = '$_POST[rideid]' ;
+          INSERT INTO bids
+          SELECT '$userid', '$_POST[rideid]', '$_POST[bid]', 0, null
+          WHERE NOT EXISTS (SELECT 1 FROM bids WHERE usersid = '$userid' and ridesid = '$_POST[rideid]');");
+          if (!$result) {
+            echo "Bid failed!!";
+          } else {
+            echo "Bid successful!";
+          }
+        }
+        else{
+          echo "Bid failed. Bid price lower than base price!";
+        }
+      }
+
+      ?>
+    </div>
 
 
 
