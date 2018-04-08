cdadde666612de5e008e52249150ed88ca522c30
Yilun Zhu <yilun95@gmail.com>
2018-04-08 21:37:22 +0800

    change bidding selection to table click

--- Webpages/bid.php
+++ Webpages/bid.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <head>
   <title>Ride bidding</title>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <style>
   .error {
     color: #FF0000;
+  }
+
+  .selected {
+    background-color: coral;
+    color: #FFF;
   }
 
   li {
@@ -23,6 +31,7 @@
   <div>
 
     <br><h1 style="text-align: center">Ride bidding info</h1> <br>
+    <h4 style="text-align: center"> Click on a ride to bid for it </h4> <br>
 
     <?php
     // Connect to the database. Please change the password in the following line accordingly
@@ -56,11 +65,10 @@
         $fieldName = pg_field_name($result, $i);
         echo '<td>' . $fieldName . '</td>';
         $i = $i + 1;
-    }
-    else {
-      echo '<td>' . "Choose a ride" . '</td>';
-      $i = $i + 1;
-    }
+      }
+      else {
+        $i = $i + 1;
+      }
     }
 
     echo '</tr>';
@@ -77,12 +85,11 @@
           $c_row = current($row);
           echo '<td>' . $c_row . '</td>';
           next($row);
-      }
-      else {
-        echo '<td>' . '<input type="button" style="text-align: center" value="Select" onclick="GetCellValues($i);">' . '</td>';
-        next($row);
-      }
-      $y = $y + 1;
+        }
+        else {
+          next($row);
+        }
+        $y = $y + 1;
       }
       echo '</tr>';
       $i = $i + 1;
@@ -94,116 +101,132 @@
     ?>
   </div>
   <div style="text-align: center">
-    <h1 style="text-align: center"> Bid for a ride </h1>
 
 
-    <form name="form1" id="form1" action="" method="POST">
-      <select name='rideid' id = 'rideid' >
-        <option value="">--- Select Ride ID ---</option>
-        <?php
-        // This is the db to connect to
-        include 'phpconfig.php';
-        $db     = $psql;
-        $result = pg_query($db, "SELECT rideid FROM rides");		// Query template
 
-
-        while ($row = pg_fetch_array($result)) {
-          echo "<option value='" . $row['rideid'] ."'>" . $row['rideid'] ."</option>";
-        }
-        ?>
-      </select>
-      <input type="submit" style="text-align: center" value="Select Ride ID" name="submitRideId"><br>
-    </form>
-
+    <!-- <form name="form1" id="form1" action="" method="POST">
+    <select name='rideid' id = 'rideid' >
+    <option value="">--- Select Ride ID ---</option>
     <?php
     // This is the db to connect to
     include 'phpconfig.php';
     $db     = $psql;
-    session_start();
-    $email = $_SESSION['userID'];
-    $result = pg_query($db, "SELECT * FROM rides where rideid = '$_POST[rideid]'");		// Query template
-    $row    = pg_fetch_assoc($result);		// To store the result row
+    $result = pg_query($db, "SELECT rideid FROM rides");		// Query template
 
-    if (isset($_POST['submitRideId'])) {
 
-      echo "
-      <form name='bid' method='POST'>
-      RideID:<br>
-      <input type='text' name='rideid' value='$row[rideid]' readonly><br>
-      Date:<br>
-      <input type='text' name='dates' value='$row[dates]' readonly><br>
-      Time:<br>
-      <input type='text' name='times' value='$row[times]' readonly><br>
-      Origin:<br>
-      <input type='text' name='origin' value='$row[origin]' readonly><br>
-      Destination:<br>
-      <input type='text' name='destination' value='$row[destination]' readonly><br>
-      Base price:<br>
-      <input type='text' name='baseprice' value='$row[baseprice]' readonly><br>
-      Your bid:<br>
-      <input type='text' name='bid' placeholder='Enter your bid' required><br>
-      Comments:<br>
-      <input type='text' name='sidenote' placeholder='Comments for driver' ><br>
-      <input type='submit' name='new'><br>
-      </form>
-      ";
-    }
-    if (isset($_POST['new'])) {	// Submit the update SQL command, update if user has already bid for ride, else insert.
-      if($_POST[bid] >= $_POST[baseprice]) {
-        $sidenote = ($_POST[sidenote] == "") ? "null" : $_POST[sidenote];
-        $result = pg_query($db, " UPDATE bids SET price = '$_POST[bid]', sidenote = '$sidenote' WHERE emails = '$email' and ridesid = '$_POST[rideid]' ;
-          INSERT INTO bids
-          SELECT '$email', '$_POST[rideid]', '$_POST[bid]', 0, '$sidenote'
-          WHERE NOT EXISTS (SELECT 1 FROM bids WHERE emails = '$email' and ridesid = '$_POST[rideid]');");
-          if (!$result) {
-            echo "Bid failed!!";
-          } else {
-            header("Refresh:0");
-            $message = "Bid successful!";
-            echo "<script type='text/javascript'>alert('$message');</script>";
-          }
-        } else {
-          echo "Bid failed. Bid price lower than base price!";
-        }
+    while ($row = pg_fetch_array($result)) {
+    echo "<option value='" . $row['rideid'] ."'>" . $row['rideid'] ."</option>";
+  }
+  ?>
+</select>
+<input type="submit" style="text-align: center" value="Select Ride ID" name="submitRideId"><br>
+</form> -->
+
+<?php
+// This is the db to connect to
+include 'phpconfig.php';
+$db     = $psql;
+session_start();
+$email = $_SESSION['userID'];
+$id = intval($_GET['id']);
+if($id > 0){
+  $result = pg_query($db, "SELECT * FROM rides where rideid = '$id'");
+}
+else{
+  $result = pg_query($db, "SELECT * FROM rides where rideid = '$_POST[rideid]'");
+
+}
+$row    = pg_fetch_assoc($result);		// To store the result row
+$location_id = filter_input(INPUT_POST, 'locationID', FILTER_SANITIZE_NUMBER_INT);
+
+
+if (isset($_POST['submitRideId']) || $id > 0) {
+
+  echo "
+  <form name='bid' method='POST'>
+  RideID:<br>
+  <input type='text' name='rideid' value='$row[rideid]' readonly><br>
+  Date:<br>
+  <input type='text' name='dates' value='$row[dates]' readonly><br>
+  Time:<br>
+  <input type='text' name='times' value='$row[times]' readonly><br>
+  Origin:<br>
+  <input type='text' name='origin' value='$row[origin]' readonly><br>
+  Destination:<br>
+  <input type='text' name='destination' value='$row[destination]' readonly><br>
+  Base price:<br>
+  <input type='text' name='baseprice' value='$row[baseprice]' readonly><br>
+  Your bid:<br>
+  <input type='text' name='bid' placeholder='Enter your bid' required><br>
+  Comments:<br>
+  <input type='text' name='sidenote' placeholder='Comments for driver' ><br>
+  <input type='submit' name='new'><br>
+  </form>
+  ";
+}
+if (isset($_POST['new'])) {	// Submit the update SQL command, update if user has already bid for ride, else insert.
+  if($_POST[bid] >= $_POST[baseprice]) {
+    $sidenote = ($_POST[sidenote] == "") ? "null" : $_POST[sidenote];
+    $result = pg_query($db, " UPDATE bids SET price = '$_POST[bid]', sidenote = '$sidenote' WHERE emails = '$email' and ridesid = '$_POST[rideid]' ;
+      INSERT INTO bids
+      SELECT '$email', '$_POST[rideid]', '$_POST[bid]', 0, '$sidenote'
+      WHERE NOT EXISTS (SELECT 1 FROM bids WHERE emails = '$email' and ridesid = '$_POST[rideid]');");
+      if (!$result) {
+        echo "Bid failed!!";
+      } else {
+        header("Refresh:0");
+        $message = "Bid successful!";
+        echo "<script type='text/javascript'>alert('$message');</script>";
       }
-
-      ?>
-    </div>
-
-
-
-  </body>
-  <script>
-  function bidFilter() {
-
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("myOrigin");
-    input2 = document.getElementById("myDest");
-    filter = input.value.toUpperCase();
-    filter2 = input2.value.toUpperCase();
-    table = document.getElementById("bidTable");
-    tr = table.getElementsByTagName("tr");
-
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 1; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[3];
-      td2 = tr[i].getElementsByTagName("td")[4];
-      if (td) {
-        if (td.innerHTML.toUpperCase().indexOf(filter) > -1 && td2.innerHTML.toUpperCase().indexOf(filter2) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
+    } else {
+      echo "Bid failed. Bid price lower than base price!";
     }
   }
 
-function GetCellValues(i) {
-    var table = document.getElementById('bidTable');
-    alert("hello ");
+  ?>
+</div>
 
+
+
+</body>
+<script>
+function bidFilter() {
+
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("myOrigin");
+  input2 = document.getElementById("myDest");
+  filter = input.value.toUpperCase();
+  filter2 = input2.value.toUpperCase();
+  table = document.getElementById("bidTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 1; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[3];
+    td2 = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1 && td2.innerHTML.toUpperCase().indexOf(filter2) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
     }
+  }
 }
+
+$("#bidTable tr").click(function(){
+  $(this).addClass('selected').siblings().removeClass('selected');
+  var value=$(this).find('td:first').html();
+  window.location = window.location.pathname + "?id=" + value;
+
+});
+
+
+
+
+
+
+
 
 </script>
 </html>
